e6a631676d59b46e700f25816aac75a280942c3d
root <charayaamit@gmail.com>
2020-12-30 14:12:16 +0000

    venv fixed

--- punjabgk/venv/bin/django-admin.py
+++ punjabgk/venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/amit/PycharmProjects/gksite/punjabgk/venv/bin/python
+#!/root/gksite/punjabgk/venv/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
